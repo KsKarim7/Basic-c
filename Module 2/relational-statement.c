1933653f363153b6b95d835aa88cdac16d02df82
Khaled Karim <kskarim177@gmail.com>
2023-04-02 22:43:28 +0600

    module 2 done

---
+++ Module 2/relational-statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // printf("Size of char : %d\n", sizeof(char));
+    // return 0;
+
+    int x = 0;
+    if (x)
+    {
+        printf("hi");
+    }
+    else
+    {
+        printf("how are u");
+    }
+}
