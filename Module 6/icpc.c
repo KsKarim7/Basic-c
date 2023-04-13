611094a9f757ca0d2fa47abe532220ac738c169b
Khaled Karim <kskarim177@gmail.com>
2023-04-13 10:48:08 +0600

    EOF

--- Module 6/icpc.c
+++ Module 6/icpc.c
@@ -17,12 +17,52 @@ int main()
     //         }
     //     }
     // }
-    int n;
-    scanf("%d", n);
-    for (int i = 1; i <= n; ++i)
-    {
+    // int n, a;
 
-        printf("Hello");
+    // scanf("%d", &n);
+    // int even = 0, odd = 0, pos = 0, neg = 0;
+    // for (int i = 1; i <= n; i = i + 1)
+    // {
+    //     scanf("%d", &a);
+    //     if (a % 2 == 0)
+    //     {
+    //         even += 1;
+    //         if (a > 0)
+    //         {
+    //             pos += 1;
+    //         }
+    //         else if (a < 0)
+    //         {
+    //             neg += 1;
+    //         }
+    //     }
+    //     else if (a % 2 != 0)
+    //     {
+    //         odd += 1;
+    //         if (a > 0)
+    //         {
+    //             pos += 1;
+    //         }
+    //         else if (a < 0)
+    //         {
+    //             neg += 1;
+    //         }
+    //     }
+    // }
+    // printf("Even %d\nOdd %d\nPositive %d\nNegative %d", even, odd, pos, neg);
+
+    int n;
+    while (scanf("%d", &n) != EOF)
+    {
+        if (n == 1999)
+        {
+            printf("Correct\n");
+            break;
+        }
+        else
+        {
+            printf("Wrong\n");
+        }
     }
     return 0;
 }
